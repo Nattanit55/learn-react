77b70298474f7e78b56e2a710d4dd4d47966b1e6
Nattanit.t <nattanit.t@socket9.com>
2023-11-01 21:09:39 +0700

    feature : add tab-menu-bar.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,45 +1,80 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Menu.css";
 
 const Menu = () => {
+  const [activeMenu, setActiveMenu] = useState()
 
-  useEffect(() => {
-    const mainMenuLi = document.getElementById("mainMenu").querySelectorAll("li");
+  // useEffect(() => {
+  //   const mainMenuLi = document.getElementById("mainMenu").querySelectorAll("li");
 
-    function changeActive() {
-      mainMenuLi.forEach((n) => n.classList.remove("active"));
-      this.classList.add("active");
-    }
+  //   function changeActive() {
+  //     mainMenuLi.forEach((n) => n.classList.remove("active"));
+  //     this.classList.add("active");
+  //   }
 
-    mainMenuLi.forEach((n) => n.addEventListener("click", changeActive));
-  }, []);
+  //   mainMenuLi.forEach((n) => n.addEventListener("click", changeActive));
+  // }, []);
 
   let imgAvatar = "../asset/media/images/icons/icon-avatar.png";
+
+  let menuList = [
+    { 
+      id:1,
+      img: "../asset/media/images/icons/icon-avatar.png",
+    },
+    { 
+      id:2,
+      img: imgAvatar,
+    },
+    { 
+      id:3,
+      img: imgAvatar,
+    },
+    { 
+      id:4,
+      img: imgAvatar,
+    },
+    { 
+      id:5,
+      img: imgAvatar,
+    }
+  ];
+
+  let bottomMenuList = [
+    { 
+      id:6,
+      img: imgAvatar,
+    },
+    { 
+      id:7,
+      img: imgAvatar,
+    },
+  ];
+
+  const IconMenu = ({ iconMenu, id }) => (
+    <li className={`${activeMenu == id && "active"}`} onClick={()=> setActiveMenu(id)}>
+      <a href="#">
+        <img className="icon-menu" src={iconMenu} alt="icon-menu" />
+      </a>
+    </li>
+  );
 
   return (
     <div className="block-menu-wrapper">
       <img className="icon-avatar" src={imgAvatar} alt="icon-avatar" />
       <ul id="mainMenu">
-        <IconMenu iconMenu={imgAvatar} />
-        <IconMenu iconMenu={imgAvatar} />
-        <IconMenu iconMenu={imgAvatar} />
-        <IconMenu iconMenu={imgAvatar} />
-        <IconMenu iconMenu={imgAvatar} />
+      {menuList.map(e => (
+        <IconMenu id={e.id} key={e.id} iconMenu={e.img} />
+      ))}
       </ul>
       <ul className="last-menu">
-        <IconMenu iconMenu={imgAvatar} />
-        <IconMenu iconMenu={imgAvatar} />
+      {bottomMenuList.map(e => (
+        <IconMenu id={e.id} key={e.id} iconMenu={e.img} />
+      ))}
       </ul>
     </div>
   );
 }
 
-const IconMenu = ({ iconMenu }) => (
-  <li>
-    <a href="#">
-      <img className="icon-menu" src={iconMenu} alt="icon-menu" />
-    </a>
-  </li>
-);
 
 export default Menu;
